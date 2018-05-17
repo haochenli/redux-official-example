1788377e854733fafe52a155c2412911812b0acf
Haochen Li <haochenli@appledeMacBook-Pro-2.local>
2018-05-17 23:11:29 +0800

    reconstruct reducer combination

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,9 @@ import { combineReducers } from 'redux'
 import todos from './todos'
 import visibilityFilter from './visibilityFilter'
 
-const todoApp = combineReducers({
-  todos,
-  visibilityFilter
-})
-
-export default todoApp
+export default function todoApp(state = {}, action) {
+  return {
+    visibilityFilter: visibilityFilter(state.visibilityFilter, action),
+    todos: todos(state.todos, action)
+  }
+}
